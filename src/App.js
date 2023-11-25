cf0c0ffc1e48e12912c773aa269deab48f92e290
Your Name <you@example.com>
2023-11-25 23:29:41 +0530

    added favourite page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Favourite from './components/Favourite';
 import { movies } from './components/Moviedata';
 import Movies from './components/Movies';
 import Navbar from './components/Navbar';
@@ -11,8 +12,8 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      
-      <Movies />
+    {/*  <Movies />*/}
+    <Favourite />
      
     </div>
   );
